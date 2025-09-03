5012c2af643450d3e83163cc6ee969ea713778b4
Erano <muhammedalisert01@gmail.com>
2025-09-03 22:01:24 +0300

    Feat: baby sheep assets loaded.

--- src/main/java/me/erano/com/model200/Application.java
+++ src/main/java/me/erano/com/model200/Application.java
@@ -1,10 +1,9 @@
-package me.erano.com;
+package me.erano.com.model200;
 
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.entity.passive.EntitySheep;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -13,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = Application.MODID, version = Application.VERSION)
 public class Application
 {
-    public static final String MODID = "Model200";
+    public static final String MODID = "model200";
     public static final String VERSION = "1.0";
     
     @EventHandler
