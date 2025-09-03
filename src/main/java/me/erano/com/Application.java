8c89ad9a2a0b99860679100dac632891d8d77808
Erano <muhammedalisert01@gmail.com>
2025-09-03 21:42:29 +0300

    Feat: baby sheep assets loaded.

---
+++ src/main/java/me/erano/com/Application.java
@@ -0,0 +1,40 @@
+package me.erano.com;
+
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Application.MODID, version = Application.VERSION)
+public class Application
+{
+    public static final String MODID = "Model200";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+        TileEntitySpecialRenderer<EntitySheep> entitySheepTileEntitySpecialRenderer = new TileEntitySpecialRenderer<EntitySheep>() {
+            @Override
+            public void renderTileEntityAt(EntitySheep te, double x, double y, double z, float partialTicks, int destroyStage) {
+
+            }
+        };
+        EntityRenderer entityRenderer = new EntityRenderer(null, null) {
+            @Override
+            public void renderWorld(float partialTicks, long finishTimeNano) {
+
+            }
+        };
+        OBJLoader.instance.addDomain(MODID);
+        ModelResourceLocation sheepModelLocation = new ModelResourceLocation("assets/model200/models/sheep.obj");
+
+    }
+
+
+}
